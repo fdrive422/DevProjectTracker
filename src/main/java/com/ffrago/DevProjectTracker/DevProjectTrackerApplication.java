d8ec87050b060a91f88300acd6d5ddfa16802293
fdrive422 <fafrago@gmail.com>
2022-06-09 22:29:37 -0700

    first commit

---
+++ src/main/java/com/ffrago/DevProjectTracker/DevProjectTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ffrago.DevProjectTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevProjectTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevProjectTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ffrago/DevProjectTracker/DevProjectTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffrago.DevProjectTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProjectTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
